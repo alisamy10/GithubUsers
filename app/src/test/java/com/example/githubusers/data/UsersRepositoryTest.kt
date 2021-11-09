7101861dd7a814f25d0592f2474737e14bfc78fb
Ali Samy <ali.samy@itworxedu.com>
2021-11-09 16:49:29 +0200

    UserRepository unit test using fake data source and default parameters

---
+++ app/src/test/java/com/example/githubusers/data/UsersRepositoryTest.kt
@@ -0,0 +1,193 @@
+package com.example.githubusers.data
+
+import com.example.githubusers.common.NetworkAwareHandler
+import com.example.githubusers.data.model.UsersResponseItem
+import com.example.githubusers.data.sources.homeCahedData.OfflineDataSource
+import com.example.githubusers.data.sources.remoteApi.OnlineDataSource
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.core.Is
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+
+class UsersRepositoryTest {
+
+val fakeList= mutableListOf<UsersResponseItem>().apply {
+        add(UsersResponseItem(
+          login="Ali",
+            id=1
+        ))
+
+        add(UsersResponseItem(
+            login="ahmed",
+            id=2
+        ))
+
+    }
+
+
+
+
+    @Test
+    fun getUsers_andInsertit_inRoom(){
+
+        runBlocking {
+            val offlineDataSource = object : OfflineDataSource {
+                override fun getLocalUsers(): List<UsersResponseItem> {
+                    return fakeList
+                }
+            }
+            val usersRepository = UsersRepositoryImpl(object : OnlineDataSource {},object : NetworkAwareHandler {}
+                ,offlineDataSource)
+
+            val result = usersRepository.getUsers()
+            val exepected = listOf(
+                UsersResponseItem(
+                    login="Ali",
+                    id=1
+                ),
+                UsersResponseItem(
+                    login="ahmed",
+                    id=2
+                )
+            )
+            assertThat(result, Is.`is`(exepected))
+        }
+    }
+
+
+    @Test
+    fun getUsers_withOnlineNetwork_thenReturnListOfSourcesFromOfflineDataSource() {
+        // run blocking to call suspend function or Coroutines scope
+        runBlocking {
+            val offlineDataSource = object : OfflineDataSource {
+                override fun getLocalUsers(): List<UsersResponseItem> {
+                    return fakeList
+                }
+            }
+            val usersRepository = UsersRepositoryImpl(object : OnlineDataSource {},
+                object : NetworkAwareHandler {},offlineDataSource)
+
+            val result = usersRepository.getUsers()
+            val exepected = listOf(
+                UsersResponseItem(
+                    login="Ali",
+                    id=1
+                ),
+                UsersResponseItem(
+                    login="ahmed",
+                    id=2
+                )
+            )
+            assertThat(result, Is.`is`(exepected))
+        }
+
+    }
+
+
+    @Test
+    fun getUsers_withOfflineNetwork_thenReturnListOfSourcesFromOfflineDataSource() {
+        runBlocking {
+            val offlineDataSource = object : OfflineDataSource {
+                override fun getLocalUsers(): List<UsersResponseItem> =fakeList
+
+            }
+
+            val networkAwareHandler =object : NetworkAwareHandler {
+                override fun isOnline(): Boolean = false
+            }
+
+            val usersRepository = UsersRepositoryImpl( object : OnlineDataSource {},
+                networkAwareHandler,
+                offlineDataSource,)
+
+
+            val result = usersRepository.getUsers()
+            val exepected = listOf(
+                UsersResponseItem(
+                    login="Ali",
+                    id=1
+                ),
+                UsersResponseItem(
+                    login="ahmed",
+                    id=2
+                )
+            )
+            assertThat(result, Is.`is`(exepected))
+        }
+
+    }
+
+    @Test
+    fun getUsers_withOnlineNetwork_verifyGetSourcesFromNetworkCalled() {
+        runBlocking {
+            var isGetSourcesInvoked = false
+
+            val onlineDataSource = object : OnlineDataSource {
+                override suspend fun getUsers(): List<UsersResponseItem> {
+                    isGetSourcesInvoked = true
+                    return listOf()
+                }
+            }
+
+            val usersRepository = UsersRepositoryImpl(onlineDataSource,
+                object : NetworkAwareHandler {},
+                object : OfflineDataSource {},
+                )
+
+            usersRepository.getUsers()
+
+            assertThat(isGetSourcesInvoked, Is.`is`(true))
+        }
+
+    }
+
+    @Test
+    fun getUsers_withOnlineNetwork_verifyCacheArticlesCalled() {
+        runBlocking {
+            var isCachedInvoked = false
+
+            val onlineDataSource = object : OnlineDataSource {
+                override suspend fun getUsers(): List<UsersResponseItem> {
+                    return listOf()
+                }
+            }
+
+            val offlineDataSource = object : OfflineDataSource {
+                override suspend fun cacheUsers(data: List<UsersResponseItem>) {
+                    isCachedInvoked = true
+                }
+            }
+
+
+            val usersRepository = UsersRepositoryImpl(
+                onlineDataSource, object : NetworkAwareHandler {}, offlineDataSource)
+
+            usersRepository.getUsers()
+
+            assertThat(isCachedInvoked, Is.`is`(true))
+        }
+    }
+
+    @Test
+    fun getUsers_withOfflineNetwork_verifyGetSourcesFromOfflineDataSourceIsCalled() {
+        runBlocking {
+            var isGetSourcesInvoked = false
+
+            val onlineDataSource = object : OnlineDataSource {
+                override suspend fun getUsers(): List<UsersResponseItem> {
+                    isGetSourcesInvoked = true
+                    return listOf()
+                }
+            }
+
+            val usersRepository = UsersRepositoryImpl(onlineDataSource,object : NetworkAwareHandler {},
+                object : OfflineDataSource {},)
+
+            usersRepository.getUsers()
+
+            assertThat(isGetSourcesInvoked, Is.`is`(true))
+        }
+    }
+}
+
